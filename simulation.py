26965ec607dc60768df5939e4d707d8eb4d29c23
Jannis145 <jannis@fruehsorge.de>
2025-08-25 16:58:02 +0200

    Schleifenstruktur erstellt

---
+++ simulation.py
@@ -0,0 +1,9 @@
+#Ruleset
+#Variablen definiert
+
+roundanzahl = 50 #Input von der Konsole
+
+for strategie in strategie_liste:
+    for strategie2 in strategie_liste:
+        for round in range(0, roundanzahl):
+            pass
