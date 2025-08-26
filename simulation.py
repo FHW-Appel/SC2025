248691a2b15c651a45840fb9d31574cc57b312ad
kvxd <89706070+0x1bd@users.noreply.github.com>
2025-08-26 11:59:51 +0200

    feat: ruleset

--- simulation.py
+++ simulation.py
@@ -1,9 +1,5 @@
-coop_coop = [3, 3]
-defect_defect = [1, 1]
-coop_defect = [5, 0]
-defect_coop = [0, 5]
-
 dict = {
+    # Strategy 1, Strategy 2 = reward strategy 1, reward strategy 2
     (True, True): [3, 3],
     (False, False): [1, 1],
     (True, False): [5, 0],
@@ -11,12 +7,6 @@ dict = {
 }
 
 def eval_ruleset(strategy_1: bool, strategy_2: bool):
-    if strategy_1 and strategy_2:
-        return coop_coop
-    if not strategy_1 and not strategy_2:
-        return defect_defect
-    if strategy_1 and not strategy_2:
-        return coop_defect
-    if not strategy_1 and strategy_2:
-        return defect_coop
+    return dict.get((strategy_1, strategy_2))
 
+print(eval_ruleset(True, False))
