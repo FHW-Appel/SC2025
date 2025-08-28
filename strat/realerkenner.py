8ec04a489ab0f7a8314b862e8a197a1a62acecef
kvxd <89706070+0x1bd@users.noreply.github.com>
2025-08-28 19:20:56 +0200

    refactor and fix from realerkernner.py

--- strat/realerkenner.py
+++ strat/realerkenner.py
@@ -12,28 +12,20 @@ class realerkenner(basisstrat):
         else:
             if history_self == history_opponent:
                 opponent = "self"
-                print("Gespielt gegen sich selbst")
             elif history_opponent[0] == True and history_opponent[1] == False and history_opponent[2] == True and history_opponent[3] == True:
                 opponent = "erkenner"
-                print("Gespielt gegen erkenner/almostsmartniceman")
             elif sum(history_opponent[0:11]) == 0:
                 opponent = "dagoberte"
-                print("Gespielt gegen dagoberte")
             elif sum(history_opponent[0:11]) == 11:
                 opponent = "stupidniceman"
-                print("Gespielt gegen stupidniceman")
             elif sum(history_opponent[0:4]) == 4 and sum(history_opponent[5:11]) == 0:
                 opponent = "nachtragend"
-                print("Gespielt gegen nachtragend")
             elif history_opponent[0] == True and history_opponent[1] == False and history_opponent[2] == True and history_opponent[3] == False and history_opponent[4] == True and history_opponent[5] == False and history_opponent[6] == True and history_opponent[7] == False and history_opponent[8] == True and history_opponent[9] == False and history_opponent[10] == True:
                 opponent = "Ping_Pong"
-                print("Gespielt gegen Ping_Pong")
             elif history_opponent[0] == False and history_opponent[1] == True and history_opponent[2] == True and history_opponent[3] == False and history_opponent[4] == False and history_opponent[5] == False and history_opponent[6] == True and history_opponent[7] == False and history_opponent[8] == False and history_opponent[9] == True and history_opponent[10] == True:
                 opponent = "bing_bong"
-                print("Gespielt gegen bing_bong")
             elif history_self[roundcounter - 11:roundcounter - 1] == history_opponent[roundcounter - 10:roundcounter]:
                 opponent = "vincentvariant"
-                print("Gespielt gegen vincentvariant")
             else:
                 opponent = "unknown"
                 if roundcounter < 20:
