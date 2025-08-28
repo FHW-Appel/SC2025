d64174c7873cd8945e9a4ce389b8162f10d9a00c
Jannis145 <jannis@fruehsorge.de>
2025-08-28 17:18:56 +0200

    Update realerkenner.py

--- strat/realerkenner.py
+++ strat/realerkenner.py
@@ -15,7 +15,7 @@ class realerkenner(basisstrat):
                 print("Gespielt gegen sich selbst")
             elif history_opponent[0] == True and history_opponent[1] == False and history_opponent[2] == True and history_opponent[3] == True:
                 opponent = "erkenner"
-                print("Gespielt gegen erkenner")
+                print("Gespielt gegen erkenner/almostsmartniceman")
             elif sum(history_opponent[0:11]) == 0:
                 opponent = "dagoberte"
                 print("Gespielt gegen dagoberte")
@@ -31,11 +31,16 @@ class realerkenner(basisstrat):
             elif history_opponent[0] == False and history_opponent[1] == True and history_opponent[2] == True and history_opponent[3] == False and history_opponent[4] == False and history_opponent[5] == False and history_opponent[6] == True and history_opponent[7] == False and history_opponent[8] == False and history_opponent[9] == True and history_opponent[10] == True:
                 opponent = "bing_bong"
                 print("Gespielt gegen bing_bong")
+            elif history_self[roundcounter - 11:roundcounter - 1] == history_opponent[roundcounter - 10:roundcounter]:
+                opponent = "vincentvariant"
+                print("Gespielt gegen vincentvariant")
             else:
                 opponent = "unknown"
-            if opponent == "self":
+                if roundcounter < 20:
+                    return self.COOPERATE
+            if opponent == "self" or opponent == "vincentvariant":
                 return self.COOPERATE
-            elif opponent == "erkenner" or opponent == "unknown" or opponent == "stupidniceman" or opponent == "dagoberte" or opponent == "nachtragend" or opponent == "Ping_Pong":
+            elif opponent == "erkenner" or opponent == "unknown" or opponent == "stupidniceman" or opponent == "dagoberte" or opponent == "nachtragend" or opponent == "Ping_Pong" or opponent == "bing_bong":
                 return self.DEFECT
             else:
                 return self.COOPERATE
