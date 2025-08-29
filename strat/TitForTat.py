1e1c1a125f1a2d8a3a10e3b7e8f120269706ba06
Jannis145 <jannis@fruehsorge.de>
2025-08-29 10:36:47 +0200

    KI ÜBERNIMMT DIE WELTHERRSCHAFT

--- strat/TitForTat.py
+++
@@ -1,9 +0,0 @@
-from basisstrat import basisstrat
-
-class TitForTat(basisstrat):
-
-    def act(self, history_self, history_opponent):
-        if len(history_self) == 0:
-            return self.COOPERATE
-        else:
-            return history_opponent[-1]
--- strat/gegenteil.py
+++
@@ -1,9 +0,0 @@
-from basisstrat import basisstrat
-
-class gegenteil(basisstrat):
-    NICE = False
-
-    def act(self, history_self, history_opponent):
-        if len(history_self) == 0:
-            return self.COOPERATE
-        return not history_opponent[-1]
